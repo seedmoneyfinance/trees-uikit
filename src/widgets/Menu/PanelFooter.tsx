373e1d184fc0bc1bb9e009a453d05726f28fcab7
Yeti <martiandefi@gmail.com>
2021-05-24 02:09:29 +0800

    - Fixed social media buttons layout at the bottom of the menu

--- src/widgets/Menu/PanelFooter.tsx
+++ src/widgets/Menu/PanelFooter.tsx
@@ -49,7 +49,7 @@ const SettingsEntry = styled.div`
 const SocialEntry = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-end;
   height: ${MENU_ENTRY_HEIGHT}px;
   padding: 0 16px;
 `;
