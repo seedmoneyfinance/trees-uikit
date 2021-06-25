aedf702ac342db48a696145999a7d598d2b27492
mrjunying98 <82926228+mrjunying98@users.noreply.github.com>
2021-06-25 19:16:43 +0800

    changes

--- src/widgets/Menu/Panel.tsx
+++ src/widgets/Menu/Panel.tsx
@@ -4,7 +4,7 @@ import PanelBody from "./PanelBody";
 import PanelFooter from "./PanelFooter";
 import { SIDEBAR_WIDTH_REDUCED, SIDEBAR_WIDTH_FULL } from "./config";
 import { PanelProps, PushedProps } from "./types";
-import { Image } from "../../components/Image";
+
 
 interface Props extends PanelProps, PushedProps {
   showMenu: boolean;
@@ -41,7 +41,7 @@ const Panel: React.FC<Props> = (props) => {
     <StyledPanel isPushed={isPushed} showMenu={showMenu}>
       <PanelBody {...props} />
       
-      
+  
       <PanelFooter {...props} />
     </StyledPanel>
   );
